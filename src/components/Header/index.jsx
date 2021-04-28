6b272ca608dcfe07c77d69b1093ec01ff0945b83
darlancostadev <d.arlancostademaria@gmail.com>
2021-04-27 23:38:38 -0300

    Create components

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Header() {
+  return (
+    <S.Wrapper>
+    <h1>Lorem</h1>
+  </S.Wrapper>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Main() {
+  return (
+      <h1>loremmm</h1>
+  );
+}
+
+export default Main;
